5e607919689274da2a8174c4f69f3dcdc06cfb8b
周文健 <z516798599@qq.com>
2025-08-21 11:35:01 +0800

    Refactor: Clean up sample app and update version
    - Remove unused imports and demo code from MainActivity
    - Add .gitignore for sikcomm module
    - Increment library version to 1.0.3

--- app/src/main/java/com/sik/comm_sample/MainActivity.kt
+++ app/src/main/java/com/sik/comm_sample/MainActivity.kt
@@ -1,13 +1,10 @@
-package com.sik.sikcomm
+package com.sik.comm_sample
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.sik.comm.core.CommBridge
-import com.sik.comm.bluetooth.BleManager
-import com.sik.comm.task.SerialTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val bleManager = BleManager(applicationContext)
-        CommBridge.initBle(bleManager)
-
-        // Demo call to library using the new task interface
-        CommBridge.sendTask(
-            SerialTask("demo", "/dev/ttyS1", byteArrayOf(0x00))
-        )
     }
 }
